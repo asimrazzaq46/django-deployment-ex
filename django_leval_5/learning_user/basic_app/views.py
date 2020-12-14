ee8540f668f62446ea6bdd4cc4158c39dc8562c7
asimrazzaq <asimrazzaq@MacBook-Pro.homenet.telecomitalia.it>
2020-12-14 04:50:09 +0100

    first

---
+++ django_leval_5/learning_user/basic_app/views.py
@@ -0,0 +1,98 @@
+from django.shortcuts import render
+from basic_app.forms import UserForm, User_profile
+# Create your views here.
+
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect , HttpResponse
+from django.contrib.auth import login, logout ,authenticate
+
+
+
+def index(request):
+    return render(request,'html/index.html')
+
+@login_required()
+def special(request):
+
+    return HttpResponse("Nice, You are loged In")
+
+
+@login_required()
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+def register(request):
+    registered =False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        profile_form = User_profile(data=request.POST)
+
+
+        if user_form.is_valid() and profile_form.is_valid():
+
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+
+            profile = profile_form.save(commit=False)
+            profile.user = user
+
+
+            if 'profile_pic' in request.FILES:
+                profile.profile_pic = request.FILES['profile_pics']
+
+            profile.save()
+
+            registered = True
+        else:
+            print(user_form.errors,profile_form.errors)
+    else:
+        user_form = UserForm()
+        profile_form = User_profile()
+
+    return render(request,'html/registration.html',{'user_form':user_form,
+                                                    'profil_form':profile_form,
+                                                    'registered':registered})
+
+
+
+def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username,password=password)
+
+#The view basic_app.views.user_login didn't return an HttpResponse object. It returned None instead.
+        if user:
+            if user.is_active:
+                login(request,user)
+                return HttpResponseRedirect(reverse('index'))
+
+            else:
+                return HttpResponse("ACCOUNT NOT ACTIVE")
+        else:
+            print("someone tried to login and failed")
+            print("username {} and password {}".format(username,password))
+            return HttpResponse("invalid login details")
+
+    else:
+        return render(request,'html/login.html',{})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
